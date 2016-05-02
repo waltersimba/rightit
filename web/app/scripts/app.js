8597459f85d97ec5ebb38fbad30e368f0ea93360
Walter Simba <walter.simba@rightit.co.za>
2016-05-02 23:09:42 +0200

    Initial commit for taxibook web angularjs project.

---
+++ web/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name taxibookApp
+ * @description
+ * # taxibookApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('taxibookApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taxibookApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the taxibookApp
+ */
+angular.module('taxibookApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
