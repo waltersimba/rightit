8363665b520d4d1a420b26ac72ed1c2392da6cd0
Walter Simba <walter.simba@rightit.co.za>
2016-05-02 22:47:49 +0200

    Added LanguageResource for i18n.

---
+++ src/main/java/com/rightit/taxibook/locale/Locales.java
@@ -0,0 +1,12 @@
+package com.rightit.taxibook.locale;
+
+import java.util.Locale;
+
+public class Locales {
+
+	private Locales() {};
+	
+	public static Locale of(String locale) {
+		return LanguageUtil.LOCALE_CACHE.getUnchecked(locale);
+	}
+}
