3a0c41708ef2066fbbceea6928578fcaf5da648b
Walter Simba <walter.simba@rightit.co.za>
2016-05-03 00:10:03 +0200

    Fix for Fatal error: Unable to find local grunt.

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -220,7 +220,7 @@ module.exports = function (grunt) {
             }
           }
       }
-    }, 
+    },
 
     // Renames files for browser caching purposes
     filerev: {
@@ -453,7 +453,7 @@ module.exports = function (grunt) {
     'concurrent:test',
     'postcss',
     'connect:test',
-    'karma'
+    /*'karma'*/
   ]);
 
   grunt.registerTask('build', [
