d9ce5b3f5ad6c6a2e1de0f7b70ce5dbf4281aeff
Walter Simba <walter.simba@rightit.co.za>
2016-08-03 12:41:23 +0200

    Code cleanup.

--- catalog/src/main/java/co/za/rightit/catalog/resources/ShoppingCartResource.java
+++ catalog/src/main/java/co/za/rightit/catalog/resources/ShoppingCartResource.java
@@ -3,7 +3,6 @@ package co.za.rightit.catalog.resources;
 import java.util.Optional;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
