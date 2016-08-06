f46bd4d0385ac0787773c21f83c8237feb055ac3
Walter Simba <walter.simba@rightit.co.za>
2016-08-06 10:08:35 +0200

    Converted LinkSerializer into a private class.

--- catalog/src/main/java/co/za/rightit/catalog/provider/ObjectMapperContextResolver.java
+++ catalog/src/main/java/co/za/rightit/catalog/provider/ObjectMapperContextResolver.java
@@ -1,12 +1,18 @@
 package co.za.rightit.catalog.provider;
 
+import java.io.IOException;
+
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Provider
@@ -26,4 +32,17 @@ public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper
     public ObjectMapper getContext(Class<?> type) {
         return mapper;
     }
+    
+    private class LinkSerializer extends JsonSerializer<Link> {
+
+    	@Override
+    	public void serialize(Link link, JsonGenerator jg, SerializerProvider sp)
+    			throws IOException, JsonProcessingException {
+    		jg.writeStartObject();
+    		jg.writeStringField("rel", link.getRel());
+    		jg.writeStringField("href", link.getUri().toString());
+    		jg.writeEndObject();
+    	}
+    }
+
 }
