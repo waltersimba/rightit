5cb645ce79796c45df6bb01095f349cb68989f18
Walter Simba <walter.simba@rightit.co.za>
2016-08-24 22:14:40 +0200

    Removed unused code.

--- catalog/src/main/java/co/za/rightit/catalog/service/ProductsCache.java
+++ catalog/src/main/java/co/za/rightit/catalog/service/ProductsCache.java
@@ -27,8 +27,9 @@ import co.za.rightit.commons.utils.Pageable;
 import co.za.rightit.commons.utils.Pagination;
 
 public class ProductsCache {
+
 	private static Logger LOGGER = LoggerFactory.getLogger(ProductsCache.class);
-	private LoadingCache<Key, List<Product>> cache;
+	private LoadingCache<Long, List<Product>> cache;
 	@Inject
 	private ProductRepository repository;
 
@@ -41,38 +42,14 @@ public class ProductsCache {
 		cache = CacheBuilder.newBuilder()
 				.refreshAfterWrite(expirationTimeMinutes, TimeUnit.MINUTES)
 				.removalListener(productRemovalListener )
-				.build(new CacheLoader<Key, List<Product>>() {
-
-					@Override
-					public List<Product> load(Key key) throws Exception {
-						return repository.findAll();
-					}
-
-					public ListenableFuture<List<Product>> reload(final Key key, List<Product> value) {
-						ExecutorService executor = Executors.newSingleThreadExecutor();
-						try {
-
-							ListenableFutureTask<List<Product>> task = ListenableFutureTask.create(new Callable<List<Product>>(){
-
-								@Override
-								public List<Product> call() throws Exception {
-									return repository.findAll();
-								}
-							});
-							executor.execute(task);
-							return task;
-						} finally {
-							executor.shutdown();
-						}
-					}
-				});
+				.build(new ProductLoader());
 	}
 
 	public Page<Product> getProducts(Pageable pageable) {
-		List<Product> products = cache.getUnchecked(new Key(1L, pageable)); 
+		List<Product> products = cache.getUnchecked(1L); 
 		return paginate(products, pageable);
 	}
-	
+
 	private Page<Product> paginate(List<Product> items, Pageable pageable) {
 		List<Product> paginatedList = items.stream()
 				.skip(pageable.getOffset())
@@ -81,59 +58,36 @@ public class ProductsCache {
 		Pagination pagination = new Pagination(pageable.getPageNumber(), items.size(), pageable.getLimit());
 		return new Page<Product>(paginatedList, pagination);
 	}
-	
-	private class Key {
-		private final Long id;
-		private final Pageable pageable;
-		
-		public Key(Long id, Pageable pageable) {
-			this.id = id;
-			this.pageable = pageable;
-		}
 
+	private final class ProductLoader extends CacheLoader<Long, List<Product>> {
 		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + getOuterType().hashCode();
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
+		public List<Product> load(Long key) throws Exception {
+			return repository.findAll();
 		}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Key other = (Key) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			return true;
-		}
+		public ListenableFuture<List<Product>> reload(final Long key, List<Product> value) {
+			ExecutorService executor = Executors.newSingleThreadExecutor();
+			try {
 
-		private ProductsCache getOuterType() {
-			return ProductsCache.this;
-		}
+				ListenableFutureTask<List<Product>> task = ListenableFutureTask.create(new Callable<List<Product>>(){
 
-		@Override
-		public String toString() {
-			return "Key [id=" + id + "]";
+					@Override
+					public List<Product> call() throws Exception {
+						return repository.findAll();
+					}
+				});
+				executor.execute(task);
+				return task;
+			} finally {
+				executor.shutdown();
+			}
 		}
-		
 	}
 
-	private RemovalListener<Key, List<Product>> productRemovalListener = new RemovalListener<Key, List<Product>>() {
+	private RemovalListener<Long, List<Product>> productRemovalListener = new RemovalListener<Long, List<Product>>() {
 
 		@Override
-		public void onRemoval(RemovalNotification<Key, List<Product>> notification) {
+		public void onRemoval(RemovalNotification<Long, List<Product>> notification) {
 			LOGGER.info("Refreshing product, productId = {}, was evicted? {}", notification.getKey(), notification.wasEvicted());
 		}
 	};
