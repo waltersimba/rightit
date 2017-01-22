d22d2aba3e5529a7d6d44541716001e10add1554
Walter Simba <walter.simba@rightit.co.za>
2017-01-22 14:34:56 +0200

    Auto register subscribers to EventService.

--- health-checks/health-checks-api/src/main/java/co/za/rightit/healthchecks/api/modules/EventModule.java
+++ health-checks/health-checks-api/src/main/java/co/za/rightit/healthchecks/api/modules/EventModule.java
@@ -4,13 +4,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import com.google.common.eventbus.AsyncEventBus;
-import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.InjectionListener;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 
 import co.za.rightit.healthchecks.api.notify.EventService;
 import co.za.rightit.healthchecks.api.notify.EventServiceImpl;
+import co.za.rightit.healthchecks.api.notify.subscriber.DeadEventSubscriber;
+import co.za.rightit.healthchecks.api.notify.subscriber.EventSubscriber;
+import co.za.rightit.healthchecks.api.notify.subscriber.HealthCheckStateListener;
 import co.za.rightit.healthchecks.api.notify.subscriber.SlackNotifier;
 import co.za.rightit.healthchecks.api.slack.Slacker;
 
@@ -18,15 +25,28 @@ public class EventModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(ExecutorService.class).toInstance(Executors.newWorkStealingPool());
+		final ExecutorService executor = Executors.newWorkStealingPool();
+		bind(ExecutorService.class).toInstance(executor);
+		final EventService eventService = new EventServiceImpl(new AsyncEventBus(executor));
+		bind(EventService.class).toInstance(eventService);
+		bindListener(Matchers.any(), new TypeListener() {
+			@Override
+			public <I> void hear(TypeLiteral<I> typeLiteral, TypeEncounter<I> typeEncounter) {
+				typeEncounter.register(new InjectionListener<I>() {
+					@Override
+					public void afterInjection(I instance) {
+						if(EventSubscriber.class.isAssignableFrom(instance.getClass())) {
+							final EventSubscriber eventSubscriber = (EventSubscriber) instance;
+							eventService.register(eventSubscriber);
+						}
+					}
+				});
+			}			
+		});
+		bind(DeadEventSubscriber.class);
+		bind(HealthCheckStateListener.class);
 	}
-			
-	@Provides
-	@Singleton
-	public EventBus asyncEventBus(ExecutorService executorService) {
-		return new AsyncEventBus(executorService);
-	}
-		
+
 	@Provides
 	@Singleton
 	public Slacker slacker() {
@@ -34,19 +54,11 @@ public class EventModule extends AbstractModule {
 				.withScreenName("healthchecks-bot")
 				.withChannel("#health-checks");
 	}
-	
-	@Provides
-	@Singleton
-	public EventService eventService(EventBus channel){
-		return new EventServiceImpl(channel);
-	}
-	
-	@Provides
-	@Singleton
-	public SlackNotifier slackNotifier(EventService eventService, Slacker slacker) {
-		final SlackNotifier notifier = new SlackNotifier(slacker);
-		eventService.register(notifier);
-		return notifier;
-	}
 
+	@Provides
+	@Singleton
+	public SlackNotifier slackNotifier(Slacker slacker) {
+		return new SlackNotifier(slacker);
+	}
+	
 }
--- health-checks/health-checks-api/src/main/java/co/za/rightit/healthchecks/api/notify/EventServiceImpl.java
+++ health-checks/health-checks-api/src/main/java/co/za/rightit/healthchecks/api/notify/EventServiceImpl.java
@@ -5,25 +5,17 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.eventbus.EventBus;
 
-import co.za.rightit.healthchecks.api.notify.subscriber.DeadEventSubscriber;
 import co.za.rightit.healthchecks.api.notify.subscriber.EventSubscriber;
-import co.za.rightit.healthchecks.api.util.Closeable;
 import co.za.rightit.healthchecks.model.events.Event;
 
-public class EventServiceImpl implements EventService, Closeable {
+public class EventServiceImpl implements EventService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EventServiceImpl.class);
 
 	private final EventBus channel;
-	/**
-	 * Handles events that could not be delivered.
-	 */
-	private final DeadEventSubscriber deadEventSubscriber;
-
+	
 	public EventServiceImpl(final EventBus channel) {
 		this.channel = channel;
-		deadEventSubscriber = new DeadEventSubscriber();
-		channel.register(deadEventSubscriber);
 	}
 
 	@Override
@@ -43,9 +35,5 @@ public class EventServiceImpl implements EventService, Closeable {
 		LOGGER.debug("posting event {}", (event != null ? event.getId() : "null"));
 		channel.post(event);
 	}
-
-	@Override
-	public void close() throws Exception {
-		channel.unregister(deadEventSubscriber);
-	}
+	
 }
